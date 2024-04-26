27080f4d8b55048ae11a0ea54d943d3de637e94a
yashinshekh <developeryasin@gmail.com>
2024-04-26 09:57:07 +0545

    af

--- moonsault_django/core/urls.py
+++ moonsault_django/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('',home,name="home")
+    path('',views.home,name="home"),
+    path('update_text/<int:pk>/', views.update_text, name='update_text'),
+    path('update_image/<int:pk>/', views.update_image, name='update_image'),
 ]
--- moonsault_django/core/views.py
+++ moonsault_django/core/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from .models import Item
 
 def home(request):
-    return render(request,'home.html')
+    items = Item.objects.all()
+
+    if not items:
+        Item.objects.create(text="cat")
+        Item.objects.create(text="dog")
+        items = Item.objects.all()
+
+    return render(request,'home.html',{'items': items})
+
+def update_text(request, pk):
+    if request.method == 'POST':
+        item = get_object_or_404(Item, pk=pk)
+        item.text = request.POST.get('text')
+        item.save()
+        return HttpResponse('Text updated successfully!', status=204)
+
+def update_image(request, pk):
+    if request.method == 'POST':
+        item = get_object_or_404(Item, pk=pk)
+        item.image = request.FILES.get('image')
+        item.save()
+        return HttpResponse('Image updated successfully!', status=204)
